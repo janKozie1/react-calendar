462beb6a99eee333355cf6aeae72ebd9db298420
janKozie1 <jankkoziel@gmail.com>
2019-08-27 12:13:43 +0200

    Updates

---
+++ precache-manifest.24f5ee72e6c05c65d9d6412516d07f5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138fb78499a34607b65f7cd261f0c815",
+    "url": "/react-hooks-todo/index.html"
+  },
+  {
+    "revision": "78ecba1802d1092b79ff",
+    "url": "/react-hooks-todo/static/js/2.6cfa83ac.chunk.js"
+  },
+  {
+    "revision": "b88a558b8a70673436b9",
+    "url": "/react-hooks-todo/static/js/main.a7ac2408.chunk.js"
+  },
+  {
+    "revision": "be611d29831306cd4a27",
+    "url": "/react-hooks-todo/static/js/runtime~main.64d82570.js"
+  },
+  {
+    "revision": "c3478f94845d68376153de579614053d",
+    "url": "/react-hooks-todo/static/media/add.c3478f94.svg"
+  },
+  {
+    "revision": "7e3d59cb798e31ae0386fa776b2c65f5",
+    "url": "/react-hooks-todo/static/media/arrow.7e3d59cb.svg"
+  },
+  {
+    "revision": "4243acedd30747e8f7476f0225a071f8",
+    "url": "/react-hooks-todo/static/media/thin_arrow.4243aced.svg"
+  },
+  {
+    "revision": "d7ee04227f87b350e100721f016c133c",
+    "url": "/react-hooks-todo/static/media/trash.d7ee0422.svg"
+  }
+]);
